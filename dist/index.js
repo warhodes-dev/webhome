4950b3c3c801a478c2e64bba38e5e41cfa780ed5
warhodes-dev <warhodes@gmail.com>
2023-01-31 11:27:23 -0500

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function helloWorld() {
+    const element = document.getElementById("hello-world");
+    if (element) {
+        element.textContent = "Hello, World!";
+    }
+}
+helloWorld();
